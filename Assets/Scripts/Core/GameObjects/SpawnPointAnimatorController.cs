c2780f69d9ec548e49b32c553fc11d0f2fc8a3b1
Vitek <vitek.fomino@bk.ru>
2020-01-19 22:05:16 +0300

    Tank spawner

---
+++ Assets/Scripts/Core/GameObjects/SpawnPointAnimatorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SpawnPointAnimatorController : MonoBehaviour
+{
+    public delegate void AnimationFinishedHandler();
+    public event AnimationFinishedHandler AnimationFinished;
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        animator.SetTrigger("StartSpawning");
+    }
+
+    void OnAnimationFinished()
+    {
+        AnimationFinished?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Tests/TestSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+    public SpawnPoint spawnPoint;
+    public GameObject tankPrefab;
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            spawnPoint.Spawn(SpawnTank);
+        }
+    }
+
+    void SpawnTank(Transform spawnPoint)
+    {
+        if (spawnPoint == null)
+            return;
+        Instantiate(tankPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
