7ae622007f8359a54018a598efe6aaf119d67d54
v.s <msey@null.net>
2020-04-25 17:40:31 +0300

    pickup mechanics

--- Assets/Scripts/Core/GameObjects/PickUp.cs
+++ Assets/Scripts/Core/GameObjects/PickUp.cs
@@ -14,4 +14,27 @@ public class PickUp : MonoBehaviour
     };
 
     public PickUpType Type;
+
+    public Sprite Sprite;
+
+    private float radius;
+
+    private void Start()
+    {
+        radius = GetComponent<CircleCollider2D>().radius;
+    }
+    void Update()
+    {
+        print(radius);
+
+        var tank = Physics2D.OverlapCircle(transform.position, radius)
+            .GetComponent<ITank>();
+
+        if (tank != null)
+        {
+            print("uspeh");
+
+            Destroy(gameObject);
+        }
+    }
 }
