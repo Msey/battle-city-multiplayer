c8742eb7ac5d1b81db9da5727ff973c222bd8c3e
Klimov Viktor <vitek.fomino@bk.ru>
2020-05-24 14:57:32 +0300

    small fix

--- Assets/Scripts/Core/Interfaces/ITank.cs
+++ Assets/Scripts/Core/Interfaces/ITank.cs
@@ -1,9 +1,11 @@
-﻿using static GameConstants;
+﻿using System.Collections.Generic;
+using static GameConstants;
 
 public interface ITank : IBulletTarget
 {
     Direction Direction { get; set; }
     bool Stopped { get; set; }
     void Shoot();
-    void OnMyBulletHit(IBullet bullet);
+    void OnMyBulletHit(IBullet bullet, List<IBulletTarget> targets);
+    bool IsDestroyed { get; }
 }
